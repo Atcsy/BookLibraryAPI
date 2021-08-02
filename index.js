e44ddaa8b03e4fa3b4c20ad92edc3fc89f0f9b38
katona.dev@gmail.com <katona.dev@gmail.com>
2021-08-02 20:15:38 +0200

    setup mongoDB and mongoose

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const connectDB = require('./config/db');
 //Routes
 const users = require('./routes/users');
 const books = require('./routes/books');
 //Env variables
-dotenv.config({ path: './config/config.env'});
+dotenv.config({ path: './config/config.env'}); 
+// Conncet DB
+connectDB();
 
 const app = express();
 
@@ -17,8 +20,15 @@ app.use('/api/v1/books', books);
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(
+const server = app.listen(
     PORT,
     console.log(`Server listening on port ${PORT}`)
 );
 
+
+//Handle unhandled Promise rejections
+process.on('unhandledRejection', (err, promise) => {
+    console.log(`Error: ${err.message}`);
+    //Close server & exit process
+    server.close(() => process.exit(1));
+});
