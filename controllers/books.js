4625e82dfb966713e1e0af2e1dd650e18a462f0e
katona.dev@gmail.com <katona.dev@gmail.com>
2021-08-03 17:18:58 +0200

    added User and Book CRUD

--- controllers/books.js
+++ controllers/books.js
@@ -1,33 +1,65 @@
 const Book = require('../models/Book');
 
 exports.getBooks = async (req, res, next) => {
-    res.status(200).json({succes:true, msg: 'Show all books'})
+    try {
+        const books = await Book.find();
+        res.status(200).json({ succes:true, data: books });
+    } catch (error) {
+        res.status(400).json({ success: false });
+    }
 };
 
 
 exports.getBook = async (req, res, next) => {
-    res.status(200).json({succes:true, msg: 'Show book'})
+    try {
+        const book = await Book.findById(req.params.id);
+
+        if (!book) {
+            return res.status(400).json( {success: false} );
+        }
+        res.status(200).json({ succes:true, data: book });
+    } catch (error) {
+        res.status(400).json({ success: false });
+    }
 };
 
 
 exports.createBook = async (req, res, next) => {
     try {
         const book = await Book.create(req.body);
-        res.status(201).json({
-            success:true,
-            data: book
-        });
+        res.status(201).json({ success: true, data: book });
     } catch (err) {
-        res.status(400).json({succes: false, error: err.message});
+        res.status(400).json({ succes: false, error: err.message });
     }
 };
 
 
 exports.updateBook = async (req, res, next) => {
-    res.status(200).json({succes:true, msg: 'Update book'})  
+    try {
+        const book = await Book.findByIdAndUpdate(req.params.id, req.body, {
+            new: true,
+            runValidators: true
+        });
+
+        if (!book) {
+            return res.status(400).json({ success: false });
+        }
+        res.status(200).json({ succes: true, data: book });
+    } catch (error) {
+        res.status(400).json({ success: false });
+    }
 };
 
 
 exports.deleteBook = async (req, res, next) => {
-    res.status(200).json({succes:true, msg: 'Delete book'})
+    try {
+        const book = await Book.findByIdAndDelete(req.params.id);
+
+        if(!book){
+            return res.status(400).json({ succes: false })
+        }
+        res.status(200).json({ success: true });
+    } catch (error) {
+        res.status(400).json({ success: false });
+    }
 };
--- controllers/users.js
+++ controllers/users.js
@@ -1,23 +1,65 @@
+const User = require('../models/User');
+
 exports.getUsers = async (req, res, next) => {
-    res.status(200).json({succes:true, msg: 'Show all users'})
+    try {
+        const users = await User.find();
+        res.status(200).json({ succes:true, data: users });
+    } catch (error) {
+        res.status(400).json({ success: false });
+    }
 };
 
 
 exports.getUser = async (req, res, next) => {
-    res.status(200).json({succes:true, msg: 'Show user'})
+    try {
+        const user = await User.findById(req.params.id);
+
+        if (!user) {
+            return res.status(400).json( {success: false} );
+        }
+        res.status(200).json({ succes:true, data: user });
+    } catch (error) {
+        res.status(400).json({ success: false });
+    }
 };
 
 
 exports.createUser = async (req, res, next) => {
-    res.status(200).json({succes:true, msg: 'Create user'})
+    try {
+        const user = await User.create(req.body);
+        res.status(201).json({ success: true, data: user });
+    } catch (err) {
+        res.status(400).json({ succes: false, error: err.message });
+    }
 };
 
 
 exports.updateUser = async (req, res, next) => {
-    res.status(200).json({succes:true, msg: 'Update user'})  
+    try {
+        const user = await User.findByIdAndUpdate(req.params.id, req.body, {
+            new: true,
+            runValidators: true
+        });
+
+        if (!user) {
+            return res.status(400).json({ success: false });
+        }
+        res.status(200).json({ succes: true, data: user });
+    } catch (error) {
+        res.status(400).json({ success: false });
+    }
 };
 
 
 exports.deleteUser = async (req, res, next) => {
-    res.status(200).json({succes:true, msg: 'Delete user'})
+    try {
+        const user = await User.findByIdAndDelete(req.params.id);
+
+        if(!user){
+            return res.status(400).json({ succes: false })
+        }
+        res.status(200).json({ success: true });
+    } catch (error) {
+        res.status(400).json({ success: false });
+    }
 };
