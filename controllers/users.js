673ced884265a1b45e65dc132c8cab0d0e02d456
katona.dev@gmail.com <katona.dev@gmail.com>
2021-08-08 13:51:35 +0200

    update user controller

--- controllers/users.js
+++ controllers/users.js
@@ -1,61 +1,54 @@
+const ErrorResponse = require('../utils/errorResponse');
+const asyncHandler = require('../middleware/asyncHandler');
 const User = require('../models/User');
 
-exports.getUsers = async (req, res, next) => {
-  try {
-    const users = await User.find();
-    res.status(200).json({ succes: true, data: users });
-  } catch (error) {
-    res.status(400).json({ success: false });
+// GET api/v1/users
+exports.getUsers = asyncHandler(async (req, res, next) => {
+  const users = await User.find();
+  res.status(200).json({ succes: true, data: users });
+});
+
+// GET api/v1/users/:id
+exports.getUser = asyncHandler(async (req, res, next) => {
+  const user = await User.findById(req.params.id);
+
+  if (!user) {
+    return next(
+      new ErrorResponse(`Resource not found with id of ${req.params.id}`, 404)
+    );
   }
-};
+  res.status(200).json({ succes: true, data: user });
+});
 
-exports.getUser = async (req, res, next) => {
-  try {
-    const user = await User.findById(req.params.id);
+// POST api/v1/users
+exports.createUser = asyncHandler(async (req, res, next) => {
+  const user = await User.create(req.body);
+  res.status(201).json({ success: true, data: user });
+});
 
-    if (!user) {
-      return res.status(400).json({ success: false });
-    }
-    res.status(200).json({ succes: true, data: user });
-  } catch (error) {
-    res.status(400).json({ success: false });
+// PUT api/v1/users/:id
+exports.updateUser = asyncHandler(async (req, res, next) => {
+  const user = await User.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+    runValidators: true,
+  });
+
+  if (!user) {
+    return next(
+      new ErrorResponse(`Resource not found with id of ${req.params.id}`, 404)
+    );
   }
-};
+  res.status(200).json({ succes: true, data: user });
+});
 
-exports.createUser = async (req, res, next) => {
-  try {
-    const user = await User.create(req.body);
-    res.status(201).json({ success: true, data: user });
-  } catch (error) {
-    res.status(400).json({ succes: false, error: error.message });
+// DELETE api/v1/users/:id
+exports.deleteUser = asyncHandler(async (req, res, next) => {
+  const user = await User.findByIdAndDelete(req.params.id);
+
+  if (!user) {
+    return next(
+      new ErrorResponse(`Resource not found with id of ${req.params.id}`, 404)
+    );
   }
-};
-
-exports.updateUser = async (req, res, next) => {
-  try {
-    const user = await User.findByIdAndUpdate(req.params.id, req.body, {
-      new: true,
-      runValidators: true,
-    });
-
-    if (!user) {
-      return res.status(400).json({ success: false });
-    }
-    res.status(200).json({ succes: true, data: user });
-  } catch (error) {
-    res.status(400).json({ success: false });
-  }
-};
-
-exports.deleteUser = async (req, res, next) => {
-  try {
-    const user = await User.findByIdAndDelete(req.params.id);
-
-    if (!user) {
-      return res.status(400).json({ succes: false });
-    }
-    res.status(200).json({ success: true });
-  } catch (error) {
-    res.status(400).json({ success: false });
-  }
-};
+  res.status(200).json({ success: true });
+});
