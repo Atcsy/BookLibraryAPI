14d2b1e6e920af494e977bbefd069034129695ee
katona.dev@gmail.com <katona.dev@gmail.com>
2021-08-08 13:43:15 +0200

    implement asyncHandler to remove try catch blocks

--- controllers/books.js
+++ controllers/books.js
@@ -1,68 +1,54 @@
 const ErrorResponse = require('../utils/errorResponse');
+const asyncHandler = require('../middleware/asyncHandler');
 const Book = require('../models/Book');
 
-exports.getBooks = async (req, res, next) => {
-  try {
-    const books = await Book.find();
-    res.status(200).json({ succes: true, data: books });
-  } catch (error) {
-    next(error);
+// GET api/v1/books
+exports.getBooks = asyncHandler(async (req, res, next) => {
+  const books = await Book.find();
+  res.status(200).json({ succes: true, data: books });
+});
+
+// GET api/v1/books/:id
+exports.getBook = asyncHandler(async (req, res, next) => {
+  const book = await Book.findById(req.params.id);
+
+  if (!book) {
+    return next(
+      new ErrorResponse(`Resource not found with id of ${req.params.id}`, 404)
+    );
   }
-};
+  res.status(200).json({ succes: true, data: book });
+});
 
-exports.getBook = async (req, res, next) => {
-  try {
-    const book = await Book.findById(req.params.id);
+// POST api/v1/books
+exports.createBook = asyncHandler(async (req, res, next) => {
+  const book = await Book.create(req.body);
+  res.status(201).json({ success: true, data: book });
+});
 
-    if (!book) {
-      return next(
-        new ErrorResponse(`Resource not found with id of ${req.params.id}`, 404)
-      );
-    }
-    res.status(200).json({ succes: true, data: book });
-  } catch (error) {
-    next(error);
+// PUT api/v1/books/:id
+exports.updateBook = asyncHandler(async (req, res, next) => {
+  const book = await Book.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+    runValidators: true,
+  });
+
+  if (!book) {
+    return next(
+      new ErrorResponse(`Resource not found with id of ${req.params.id}`, 404)
+    );
   }
-};
+  res.status(200).json({ succes: true, data: book });
+});
 
-exports.createBook = async (req, res, next) => {
-  try {
-    const book = await Book.create(req.body);
-    res.status(201).json({ success: true, data: book });
-  } catch (error) {
-    next(error);
+// DELETE api/v1/books/:id
+exports.deleteBook = asyncHandler(async (req, res, next) => {
+  const book = await Book.findByIdAndDelete(req.params.id);
+
+  if (!book) {
+    return next(
+      new ErrorResponse(`Resource not found with id of ${req.params.id}`, 404)
+    );
   }
-};
-
-exports.updateBook = async (req, res, next) => {
-  try {
-    const book = await Book.findByIdAndUpdate(req.params.id, req.body, {
-      new: true,
-      runValidators: true,
-    });
-
-    if (!book) {
-      return next(
-        new ErrorResponse(`Resource not found with id of ${req.params.id}`, 404)
-      );
-    }
-    res.status(200).json({ succes: true, data: book });
-  } catch (error) {
-    next(error);
-  }
-};
-
-exports.deleteBook = async (req, res, next) => {
-  try {
-    const book = await Book.findByIdAndDelete(req.params.id);
-
-    if (!book) {
-      return next(
-        new ErrorResponse(`Resource not found with id of ${req.params.id}`, 404)
-      );
-    }
-    res.status(200).json({ success: true });
-  } catch (error) {
-    next(error);
-  }
-};
+  res.status(200).json({ success: true });
+});
