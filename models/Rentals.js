4a804e31b86ca2f9718d327fa4bfe7c0b332c51d
katona.dev@gmail.com <katona.dev@gmail.com>
2021-08-08 13:23:17 +0200

    implement RetalSchema and model for rentals

---
+++ models/Rentals.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+
+const RentalSchema = new mongoose.Schema({
+  book: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Book',
+  },
+  issueDate: {
+    type: Date,
+    default: Date.now(),
+  },
+  returnDate: { type: Date },
+  id: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+  },
+});
+
+module.exports = mongoose.model('Rental', RentalSchema);
