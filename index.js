8abf5797efa6eb65ded7d56b08ae4ac90ab0b2ae
katona.dev@gmail.com <katona.dev@gmail.com>
2021-08-16 18:06:38 +0200

    added cors

--- index.js
+++ index.js
@@ -1,27 +1,29 @@
-const express = require('express');
-const dotenv = require('dotenv');
-const rateLimit = require('express-rate-limit');
-const helmet = require('helmet');
-const mongoSanitize = require('express-mongo-sanitize');
-const xss = require('xss-clean');
-const hpp = require('hpp');
-const errorHandler = require('./middleware/error');
-const connectDB = require('./config/db');
+const express = require("express");
+const dotenv = require("dotenv");
+const rateLimit = require("express-rate-limit");
+const helmet = require("helmet");
+const mongoSanitize = require("express-mongo-sanitize");
+const xss = require("xss-clean");
+const hpp = require("hpp");
+const errorHandler = require("./middleware/error");
+const connectDB = require("./config/db");
+const cors = require("cors");
 //Routes
-const auth = require('./routes/auth');
-const users = require('./routes/users');
-const books = require('./routes/books');
-const rentals = require('./routes/rentals');
+const auth = require("./routes/auth");
+const users = require("./routes/users");
+const books = require("./routes/books");
+const rentals = require("./routes/rentals");
 //Env variables
-dotenv.config({ path: './config/config.env' });
+dotenv.config({ path: "./config/config.env" });
 // Conncet DB
 connectDB();
 
 const app = express();
 
+app.use(cors());
 //Set security HTTP headers
 app.use(helmet());
-app.use(express.json({ limit: '10kb' }));
+app.use(express.json({ limit: "10kb" }));
 
 //Sanitize Data against NoSQL query injection
 app.use(mongoSanitize());
@@ -36,23 +38,23 @@ app.use(hpp());
 const limiter = rateLimit({
   max: 200,
   windowsMs: 60 * 60 * 1000,
-  message: 'Too many request in 1 hour from your IP address',
+  message: "Too many request in 1 hour from your IP address",
 });
 
 app.use(limiter);
 
 //Mount routers
-app.use('/api/v1/', auth);
-app.use('/api/v1/users', users);
-app.use('/api/v1/books', books);
-app.use('/api/v1/rentals', rentals);
+app.use("/api/v1/", auth);
+app.use("/api/v1/users", users);
+app.use("/api/v1/books", books);
+app.use("/api/v1/rentals", rentals);
 
 app.use(errorHandler);
 
-app.all('*', (req, res, next) => {
+app.all("*", (req, res, next) => {
   res
     .status(404)
-    .json({ succes: false, message: 'This route does not exists' });
+    .json({ succes: false, message: "This route does not exists" });
 });
 
 const PORT = process.env.PORT || 5000;
@@ -63,7 +65,7 @@ const server = app.listen(
 );
 
 //Handle unhandled Promise rejections
-process.on('unhandledRejection', (err) => {
+process.on("unhandledRejection", (err) => {
   console.log(`Error: ${err.message}`);
   //Close server & exit process
   server.close(() => process.exit(1));
